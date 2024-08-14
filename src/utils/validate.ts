22437609e9be48d239224120b61e53f7229fdc4b
y00eunji <aine0227@naver.com>
2024-08-14 14:26:00 +0900

    feat: 입력값의 유효성 검사 추가

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,6 +1,6 @@
 function isValidExpression(expression: string) : boolean{
-    // 숫자 또는 -,+,/,* 로 구성되어있는지 확인
-    return /^[0-9+\-*/]+$/.test(expression);
+    // 숫자 또는 -,+,/,* 로 구성되어있는지 확인, 숫자로 시작하고 숫자로 끝나야 한다
+    return /^[0-9][0-9+\-*/]*[0-9]$/.test(expression);
 }
 
 export default isValidExpression;
