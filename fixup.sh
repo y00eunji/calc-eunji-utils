d161516bb76a97fe9a1fdf567a5c45cf5855e069
y00eunji <aine0227@naver.com>
2024-08-14 11:56:51 +0900

    setting: 타입스크립트 설정

---
+++ fixup.sh
@@ -0,0 +1,17 @@
+#index.d.ts 파일을 dist 아래 경로로 옮기고 (중복 제거) 빌드된 파일들이 저장될 디렉토리에 package.json을 생성하는 스크립트이다.
+cp dist/mjs/index.d.ts dist
+
+rm -rf dist/*/index.d.ts
+
+cat >dist/cjs/package.json <<!EOF
+{
+    "type": "commonjs"
+}
+!EOF
+
+cat >dist/mjs/package.json <<!EOF
+{
+    "type": "module"
+}
+!EOF
+
