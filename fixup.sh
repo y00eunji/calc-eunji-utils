b297f545ad3a9cafc21429def24bfd662dfae47f
y00eunji <aine0227@naver.com>
2024-08-18 20:21:29 +0900

    chore :  주석 삭제

--- fixup.sh
+++ fixup.sh
@@ -14,28 +14,3 @@ cat >dist/mjs/package.json <<!EOF
     "type": "module"
 }
 !EOF
-#!/bin/bash
-
-# dist/mjs/index.d.ts 파일을 dist로 복사
-#cp dist/mjs/index.d.ts dist
-#
-## 중복 제거: dist 하위의 모든 index.d.ts 파일 삭제
-#rm -rf dist/*/index.d.ts
-#
-## cjs 및 mjs 디렉토리 생성 (존재하지 않을 경우)
-#mkdir -p dist/cjs dist/mjs
-#
-## dist/cjs/package.json 생성
-#cat > dist/cjs/package.json <<!EOF
-#{
-#    "type": "commonjs"
-#}
-#!EOF
-#
-## dist/mjs/package.json 생성
-#cat > dist/mjs/package.json <<!EOF
-#{
-#    "type": "module"
-#}
-#!EOF
-#
