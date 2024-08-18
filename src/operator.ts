310c05df69700caca750873782ecd10f6fe0ef88
y00eunji <aine0227@naver.com>
2024-08-18 18:21:40 +0900

    feat : 사칙연산 수행 함수

---
+++ src/operator.ts
@@ -0,0 +1,34 @@
+// 연산 수행을 위한 함수 모듈입니다.
+export function calculateWithOperator(prev: number, next: number, operator: string): number {
+    switch (operator) {
+        case '+':
+            return add(prev, next);
+        case '-':
+            return subtract(prev, next);
+        case '*':
+            return multiply(prev, next);
+        case '/':
+            return divide(prev, next);
+        default:
+            throw new Error(`잘못된 연산자: ${operator}`);
+    }
+}
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function divide(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("0으로 나눌 없습니다.");
+    }
+    return a / b;
+}
