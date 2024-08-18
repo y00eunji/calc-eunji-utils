407e118104589e09f2878acdaa9ca4b6fcf466d4
y00eunji <aine0227@naver.com>
2024-08-18 18:22:07 +0900

    feat : 계산기 로직

---
+++ src/evaluator.ts
@@ -0,0 +1,46 @@
+// 계산을 수행하는 로직을 포함합니다. 연산자에 따라 우선순위를 처리하고 최종 결과를 반환합니다.
+import { parseExpression } from './parser';
+import { calculateWithOperator } from './operator';
+
+export function evaluateExpression(expressionArr: (number | string)[]): number {
+    const highPriorityOps: (number | string)[] = [];
+    let i = 0;
+
+    while (i < expressionArr.length) {
+        const current = expressionArr[i];
+
+        if (typeof current === 'number') {
+            highPriorityOps.push(current);
+        } else if (current === '*' || current === '/') {
+            const prevNumber = highPriorityOps.pop() as number;
+            const nextNumber = expressionArr[++i] as number;
+            const result = calculateWithOperator(prevNumber, nextNumber, current);
+            highPriorityOps.push(result);
+        } else {
+            highPriorityOps.push(current);
+        }
+        i++;
+    }
+
+    return computeFinalResult(highPriorityOps);
+}
+
+function computeFinalResult(stack: (number | string)[]): number {
+    let result = stack.shift() as number;
+
+    while (stack.length > 0) {
+        const operator = stack.shift() as string;
+        const nextNumber = stack.shift() as number;
+
+        result = calculateWithOperator(result, nextNumber, operator);
+    }
+
+    return result;
+}
+
+export default (expression: string): number => {
+    const operators = ['+', '-', '*', '/'];
+    const expressionArr = parseExpression(expression, operators);
+    return evaluateExpression(expressionArr);
+};
+
