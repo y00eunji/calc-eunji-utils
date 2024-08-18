ad8d7512e579a1be621cd05e70e69fea979a2a73
y00eunji <aine0227@naver.com>
2024-08-18 20:00:01 +0900

    feat: 유효성 검사 조건 변경

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import isValidExpression from "./validate.js";
 import evaluator from "./evaluator.js";
 
 export function Calculator(expression: string): number {
-    if(isValidExpression(expression)) throw new Error("올바른 식이 아님")
+    if(!isValidExpression(expression)) throw new Error("올바른 식이 아님")
 
     return evaluator(expression);
 }
