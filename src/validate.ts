6195cafd29d85ac43b8df97ff749fc70b24c1a54
y00eunji <aine0227@naver.com>
2024-08-18 18:21:25 +0900

    chore : 파일 이동

---
+++ src/validate.ts
@@ -0,0 +1,5 @@
+//유효성 체크 :  숫자 또는 -,+,/,* 로 구성되어있는지 확인, 숫자로 시작하고 숫자로 끝나야 한다
+export default function isValidExpression(expression: string) : boolean{
+    return /^[0-9][0-9+\-*/]*[0-9]$/.test(expression);
+}
+
