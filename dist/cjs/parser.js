273e65932f398e7af266252ded4eeeb08b87e621
y00eunji <aine0227@naver.com>
2024-08-19 17:47:24 +0900

    chore: 로직 리팩토링

--- dist/cjs/parser.js
+++ dist/cjs/parser.js
@@ -3,5 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseExpression = parseExpression;
 // 표현식을 파싱하는 함수입니다.
 function parseExpression(expression, operators) {
-    return expression.match(/-?\d+|[+\-*/]/g).map(item => operators.includes(item) ? item : Number(item));
+    const parsed = [];
+    const tokens = expression.match(/(\d+(\.\d+)?|\D)/g);
+    if (!tokens)
+        return parsed;
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        // 음수인지 확인
+        if (token === '-' && (i === 0 || operators.includes(tokens[i - 1]))) {
+            parsed.push(-Number(tokens[++i]));
+        }
+        else if (operators.includes(token)) {
+            parsed.push(token);
+        }
+        else {
+            parsed.push(Number(token));
+        }
+    }
+    return parsed;
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,22 @@
 // 표현식을 파싱하는 함수입니다.
 export function parseExpression(expression: string, operators: string[]): (number | string)[] {
-    return expression.match(/-?\d+|[+\-*/]/g)!.map(item =>
-        operators.includes(item) ? item : Number(item)
-    );
+    const parsed: (number | string)[] = [];
+    const tokens = expression.match(/(\d+(\.\d+)?|\D)/g);
+
+    if (!tokens) return parsed;
+
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+
+        // 음수인지 확인
+        if (token === '-' && (i === 0 || operators.includes(tokens[i - 1]))) {
+            parsed.push(-Number(tokens[++i]));
+        } else if (operators.includes(token)) {
+            parsed.push(token);
+        } else {
+            parsed.push(Number(token));
+        }
+    }
+
+    return parsed;
 }
