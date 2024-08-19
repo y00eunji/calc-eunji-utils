11ae3ae0459a5c5f202405a99f589d478715eafe
y00eunji <aine0227@naver.com>
2024-08-19 09:49:28 +0900

    chore: 로직 리팩토링

--- dist/mjs/parser.js
+++ dist/mjs/parser.js
@@ -1,4 +1,12 @@
 // 표현식을 파싱하는 함수입니다.
 export function parseExpression(expression, operators) {
-    return expression.match(/-?\d+|[+\-*/]/g).map(item => operators.includes(item) ? item : Number(item));
+    return expression.match(/-?\d+|[+\-*/]/g).map(item => {
+        if (operators.includes(item)) {
+            return item;
+        }
+        else {
+            const num = Number(item);
+            return num === -0 ? 0 : num;
+        }
+    });
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,11 @@
 // 표현식을 파싱하는 함수입니다.
 export function parseExpression(expression: string, operators: string[]): (number | string)[] {
-    return expression.match(/-?\d+|[+\-*/]/g)!.map(item =>
-        operators.includes(item) ? item : Number(item)
-    );
+    return expression.match(/-?\d+|[+\-*/]/g)!.map(item => {
+        if (operators.includes(item)) {
+            return item;
+        } else {
+            const num = Number(item);
+            return num === -0 ? 0 : num;
+        }
+    });
 }
